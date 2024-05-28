90b7eb7a12c80c5d50495800bc1fde9950e49fdd
Sameh ElGazar <selgazar95@gmail.com>
2024-05-29 01:55:25 +0300

    11

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,7 +3,7 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({
         status: 200,
-        body: 'photo-profile-1',
+        body: 'Success',
       });
     } else {
       reject(new Error('The fake API is not working currently'));
