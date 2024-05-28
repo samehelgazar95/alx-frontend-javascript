9eeaa9ff6db239b8880d666eb102e7170060186a
Sameh ElGazar <selgazar95@gmail.com>
2024-05-29 02:43:07 +0300

    22

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   promise
     .then(() => ({ status: 200, body: 'Success' }))
-    .catch((err) => err)
+    .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
