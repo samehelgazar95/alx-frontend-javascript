d52b434600243b49a8c1bc23b54dac140b78489f
Sameh ElGazar <selgazar95@gmail.com>
2024-05-28 03:02:29 +0300

    11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return {
-    departmentName: [...employees],
+  const obj = {
+    [departmentName]: [...employees],
   };
+
+  return obj;
 }
