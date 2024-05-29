9f55bc2b5c1512fbf55adf98df634c1dd12587cc
Sameh ElGazar <selgazar95@gmail.com>
2024-05-29 04:00:04 +0300

    333

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.allSettled([uploadPhoto(), createUser()])
+  return Promise.allSettled([uploadPhoto(), createUser()])
     .then((results) => {
       const photoInfo = results[0].value.body;
       const userInfo = results[1].value;
