ba1fe8ef7237af2142c2ea2142359a3ef5c3167c
Sameh ElGazar <selgazar95@gmail.com>
2024-05-29 04:32:25 +0300

    6666

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((res) => {
-    return res;
+  ]).then((result) => {
+    result.map((res) => ({
+      status: res.status,
+      value: String(res.reason),
+    }));
   });
 }
