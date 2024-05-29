82bd5a0c4783792ee2889115040ab8fe24d58345
Sameh ElGazar <selgazar95@gmail.com>
2024-05-29 04:59:22 +0300

    8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,7 @@
 export default function divideFunction(numerator, denominator) {
   try {
     if (denominator === 0) throw new Error('cannot divide by 0');
-    return numerator / denominator;
+    else return numerator / denominator;
   } catch (err) {
     return err;
   }
