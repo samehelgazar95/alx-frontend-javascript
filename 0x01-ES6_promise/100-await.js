c48df4d725fd6c451588b516a1374ed032357c6b
Sameh ElGazar <selgazar95@gmail.com>
2024-05-29 19:59:04 +0300

    100.10

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,18 +1,27 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const finalResponses = {};
+  let finalResponses = {};
 
-  await Promise.allSettled([uploadPhoto(), createUser()]).then((result) =>
-    result.map((ele, index) => {
-      if (ele.status === 'fulfilled') {
-        if (index === 0) finalResponses.photo = ele.value;
-        else if (index === 1) finalResponses.user = ele.value;
-      } else {
-        if (index === 0) finalResponses.photo = null;
-        else if (index === 1) finalResponses.user = null;
-      }
-    }),
-  );
+  //   await Promise.allSettled([uploadPhoto(), createUser()]).then((result) =>
+  //     result.map((ele, index) => {
+  //       if (ele.status === 'fulfilled') {
+  //         if (index === 0) finalResponses.photo = ele.value;
+  //         else if (index === 1) finalResponses.user = ele.value;
+  //       } else {
+  //         if (index === 0) finalResponses.photo = null;
+  //         else if (index === 1) finalResponses.user = null;
+  //       }
+  //     }),
+  //   );
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    finalResponses = { photo, user };
+  } catch (err) {
+    finalResponses = { photo: null, user: null };
+  }
+
   return finalResponses;
 }
