01fa29626665e42b6885d081f9f42f621d6277f1
Sameh ElGazar <selgazar95@gmail.com>
2024-05-31 00:32:42 +0300

    4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,49 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    Pricing.isNumber(amount);
+    Pricing.isCurrency(currency);
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  static isNumber(val) {
+    if (typeof val !== 'number') throw new TypeError();
+  }
+
+  static isCurrency(val) {
+    if (val instanceof Currency === false) throw new TypeError();
+  }
+
+  set amount(val) {
+    Pricing.isNumber(val);
+    this._amount = val;
+  }
+
+  set currency(val) {
+    Pricing.isCurrency(val);
+    this.currency = val;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this.currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    Pricing.isNumber(amount);
+    Pricing.isNumber(conversionRate);
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
