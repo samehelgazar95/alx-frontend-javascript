a77f27caeccdf97694c2eb4e431ccd0318ec9ea8
Sameh ElGazar <selgazar95@gmail.com>
2024-06-04 19:08:16 +0300

    5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,8 @@ function createInt8TypedArray(length, position, value) {
   const typedInt8 = new Int8Array(buffer);
 
   if (position > typedInt8.length) throw new Error('Position outside range');
-  else typed[position] = value;
+  else typedInt8[position] = value;
+
   const view = new DataView(buffer);
 
   return view;
