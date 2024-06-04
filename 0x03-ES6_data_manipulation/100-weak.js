f99555e5cb7f119d7e00e84c2947aa8f7072123c
Sameh ElGazar <selgazar95@gmail.com>
2024-06-04 21:55:00 +0300

    100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,6 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const load = weakMap.get(endpoint) || 0;
 
-  if (load >= 5) throw new Error('Endpoint load is high');
-
   weakMap.set(endpoint, load + 1);
+  if (load >= 5) throw new Error('Endpoint load is high');
 }
