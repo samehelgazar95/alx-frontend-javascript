5beaac9a186b8cc2e7f17cbf6c8f818963277379
Sameh ElGazar <selgazar95@gmail.com>
2024-06-04 18:53:19 +0300

    4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,11 +2,13 @@ function updateStudentGradeByCity(students, city, newGrades) {
   const combined = students
     .filter((ele) => ele.location === city)
     .map((ele) => {
-      for (const grade of newGrades) {
-        if (ele.id === grade.studentId) return { ...ele, grade: grade.grade };
+      let grade = 'N/A';
 
-        return { ...ele, grade: 'N/A' };
+      for (const g of newGrades) {
+        if (ele.id === g.studentId) grade = g.grade;
       }
+
+      return { ...ele, grade };
     });
   return combined;
 }
