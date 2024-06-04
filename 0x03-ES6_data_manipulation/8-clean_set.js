b6181b56c20c67483cb883f04f5f1c5a96b29e04
Sameh ElGazar <selgazar95@gmail.com>
2024-06-04 20:54:20 +0300

    8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,15 @@
 function cleanSet(set, str) {
   const res = [];
 
-  if (str) {
-    for (const val of set) {
-      if (val.toLowerCase().startsWith(str.toLowerCase())) {
-        res.push(val.slice(str.length));
-      }
+  if (typeof str !== 'string' || str === '') return '';
+
+  for (const val of set) {
+    if (val.toLowerCase().startsWith(str.toLowerCase())) {
+      res.push(val.slice(str.length));
     }
-    return res.join('-');
   }
 
-  return res.join('');
+  return res.join('-');
 }
 
 export default cleanSet;
